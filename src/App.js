2356aead849788df6303b8cbc4604b04255861a1
sohan110111 <sohan110111@gmail.com>
2020-08-13 19:34:55 +0600

    Set log user in state, display logged in user info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 function App() {
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, photoURL, email} = res.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name : displayName,
+        email : email,
+        photo : photoURL
+      }
+      setUser(signedInUser);
+      console.log(displayName, email, photoURL);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.message);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = {handleSignIn}>Sign in</button>
+      {
+        user.isSignedIn && <p> Welcome, {user.name}</p>
+      }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPACazI_F92YHKKQesH2KnEtAffySHoPI",
+    authDomain: "ema-john-sample.firebaseapp.com",
+    databaseURL: "https://ema-john-sample.firebaseio.com",
+    projectId: "ema-john-sample",
+    storageBucket: "ema-john-sample.appspot.com",
+    messagingSenderId: "582009388210",
+    appId: "1:582009388210:web:515fe758d7582e93729dab"
+  };
+
+  export default firebaseConfig;
